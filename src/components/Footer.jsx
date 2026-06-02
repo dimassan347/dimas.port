1c212fac1d831deaed3d156b28a1624a1aeac08e
dimassan347 <dimas.sentosa86@gmail.com>
2026-06-02 15:30:45 +0700

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-center" style={{ color: 'var(--color-text-muted)' }}>
           © {currentYear}{" "}
-          <a href="https://asutrisna.dev" className="hover:underline">
-            Asep Sutrisna Suhada Putra
+          <a href="https://www.linkedin.com/in/dimas-santoso-a9b449227" className="hover:underline">
+            Dimas Santoso
           </a>
           . All Rights Reserved.
         </span>
